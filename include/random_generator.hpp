653983cda273355772d133ce6bedb7e07c4ee237
kfsworks <kfsworks@gmail.com>
2019-05-28 18:22:29 +0800

    S

--- include/random_generator.hpp
+++ include/random_generator.hpp
@@ -2,13 +2,24 @@
 #ifndef RANDOM_GENERATOR_H_
 #define RANDOM_GENERATOR_H_
 
+#include <memory>
+#include <time.h>
+#include <boost/random/normal_distribution.hpp>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/variate_generator.hpp>
+
+using namespace std;
+
 class RandomGenerator {
   private:
     static RandomGenerator* instance;
+    //shared_ptr<boost::variate_generator<boost::mt19937, boost::normal_distribution<>>> pGenerator_;
+
     RandomGenerator();
 
   public:
     static RandomGenerator* getInstance();
+    double getDouble();
 };
 
 #endif // RANDOM_GENERATOR_H_
--- src/random_generator.cpp
+++ src/random_generator.cpp
@@ -1,12 +1,29 @@
 #include "random_generator.hpp"
+#include <memory>
+#include <time.h>
+#include <boost/random/normal_distribution.hpp>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/variate_generator.hpp>
+
+using namespace std;
 
 RandomGenerator* RandomGenerator::getInstance() {
-  if (instance == 0) {
-    instance = new RandomGenerator();
+  if (RandomGenerator::instance == 0) {
+      RandomGenerator::instance = new RandomGenerator();
   }
 
-  return instance;
+  return RandomGenerator::instance;
 }
 
 RandomGenerator::RandomGenerator() {
+/*  pGenerator_ = 
+    std::make_shared<
+      boost::variate_generator<boost::mt19937, boost::normal_distribution<>>
+      >( boost::mt19937(time(0)), boost::normal_distribution<>() )
+  ;*/
+}
+
+double RandomGenerator::getDouble() {
+  //return pGenerator_->distribution();
+  return 0.2;
 }
