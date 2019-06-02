f2c954ae00a6a59392ccc963c04de5e5277b4539
kfsworks <kfsworks@gmail.com>
2019-06-02 21:21:12 +0800

    S

--- src/candidate.cpp
+++ src/candidate.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
-Candidate::Candidate(int init_x, int init_y) {
+Candidate::Candidate(std::vector<int> chromosome) {
+  chromosome_ = chromosome;
 }
 
+Candidate::Candidate() {
+  int length = 10;
+  for (int i {} ; i < length; i++) {
+    chromosome_.push_back(i+1);
+  }
+}
