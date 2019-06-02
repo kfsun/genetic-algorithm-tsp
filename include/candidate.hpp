a970449b60e8ce810cabbdc0e12f6dac1c202dff
kfsworks <kfsworks@gmail.com>
2019-06-02 09:28:37 +0800

    S

--- include/candidate.hpp
+++ include/candidate.hpp
@@ -10,19 +10,11 @@ class Candidate {
   private:
     std::vector<int> chromosome_;
     double fitness_ {-1.0};
-    int x {}, y {};
 
   public:
     Candidate() = default;
     Candidate(int init_x, int init_y);
 
-    inline int getX() {
-      return x;
-    }
-
-    inline int getY() {
-      return y;
-    }
 
 };
 
--- src/candidate.cpp
+++ src/candidate.cpp
@@ -3,7 +3,5 @@
 using namespace std;
 
 Candidate::Candidate(int init_x, int init_y) {
-  x = init_x;
-  y = init_y;
 }
 
