755010f13acf9f90372a8f0f71634c5381d9d715
kfsworks <kfsworks@gmail.com>
2019-06-04 17:15:58 +0800

    S

--- include/candidate_collection.hpp
+++ include/candidate_collection.hpp
@@ -1,16 +1,16 @@
 #ifndef CANDIDATE_COLLECTION_H_
 #define CANDIDATE_COLLECTION_H_
 
-#include <iostream>
 #include <vector>
-
-using namespace std;
+#include <memory>
+#include "candidate.hpp"
 
 class CandidateCollection {
   private:
+    std::vector<std::shared_ptr<Candidate>> candidates_;
 
   public:
-    CandidateCollection();
+    CandidateCollection(size_t num_of_candidate, size_t num_of_city);
 
 
 };
--- src/candidate_collection.cpp
+++ src/candidate_collection.cpp
@@ -1,8 +1,13 @@
 #include <vector>
 #include <memory>
 #include "candidate_collection.hpp"
+#include "candidate.hpp"
 
-
-CandidateCollection::CandidateCollection() {
+CandidateCollection::CandidateCollection(size_t num_of_candidate, size_t num_of_city) {
+  for (size_t i {}; i < num_of_candidate; i++) {
+    candidates_.push_back(
+      std::make_shared<Candidate>(num_of_city)
+    );
+  }
 }
 
